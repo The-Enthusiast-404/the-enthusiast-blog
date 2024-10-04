47378a322632ba73fa86045f389eb1976b5ecb44
Sahil <issahil404@gmail.com>
2024-10-04 23:48:17 +0530

    added introduction to linux article

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -2,22 +2,13 @@ import { slugifyStr } from "./slugify";
 import type { CollectionEntry } from "astro:content";
 import postFilter from "./postFilter";
 
-interface Tag {
-  tag: string;
-  tagName: string;
-}
-
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  const tags: Tag[] = posts
-    .filter(postFilter)
+  const filteredPosts = posts.filter(postFilter);
+  const tags: string[] = filteredPosts
     .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
-    .filter(
-      (value, index, self) =>
-        self.findIndex(tag => tag.tag === value.tag) === index
-    )
-    .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
-  return tags;
+    .map(tag => tag.toLowerCase());
+  const uniqueTags = [...new Set(tags)];
+  return uniqueTags;
 };
 
 export default getUniqueTags;
